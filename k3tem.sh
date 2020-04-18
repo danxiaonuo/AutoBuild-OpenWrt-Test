044b1f9fe88e91c36733bef5ca88194f54681f61
danxiaonuo <danxiaonuo@danxiaonuo.me>
2020-04-18 13:05:44 +0800

    Update k3tem.sh

--- k3tem.sh
+++ k3tem.sh
@@ -26,8 +26,6 @@ echo "Installing feeds"
 sed -i 's|^TARGET_|# TARGET_|g; s|# TARGET_DEVICES += phicomm-k3|TARGET_DEVICES += phicomm-k3|' target/linux/bcm53xx/image/Makefile
 sed -i '/<div class="tr"><div class="td left" width="33%"><%:Kernel Version%></div><div class="td left"><%=unameinfo.release or "?"%></div></div>/a <div class="tr"><div class="td left" width="33%"><%:CPU Temperature%></div><div class="td left"><%=luci.sys.exec("cut -c1-2 /sys/class/thermal/thermal_zone0/temp")%></div></div>' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
 
-# 移除不用软件包
-rm -rf k3screenctrl package/lean
 # 添加额外软件包
 # k3
 git clone https://github.com/lwz322/luci-app-k3screenctrl.git package/k3/luci-app-k3screenctrl
